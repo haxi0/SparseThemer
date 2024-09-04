cdaf6520dd28d7df51cdb6bc8c803ef8775f2cfb
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-09-04 23:40:06 +0800

    libpartial

---
+++ POColdsdk/libpartial/assetsgrabber.h
@@ -0,0 +1,12 @@
+//
+//  assetsgrabber.h
+//  POColdsdk
+//
+//  Created by LL on 9/4/24.
+//
+
+#ifndef assetsgrabber_h
+#define assetsgrabber_h
+
+
+#endif /* assetsgrabber_h */
---
+++ POColdsdk/libpartial/assetsgrabber.m
@@ -0,0 +1,25 @@
+//
+//  libpartial.m
+//  POColdsdk
+//
+//  Created by LL on 9/4/24.
+//
+
+#import <Foundation/Foundation.h>
+#import "partial.h"
+// Code skidded from libgrabkernel2 but changed.
+bool grabAssetsCar(NSString* zipURL, NSString* appName) {
+    NSError *error = nil;
+    Partial *zip = [Partial partialZipWithURL:[NSURL URLWithString:zipURL] error:&error];
+    if (!zip) {
+        NSLog(@"Failed to open zip file! %s\n", error.localizedDescription.UTF8String);
+        return false;
+    }
+    NSData *assetsData = [zip getFileForPath:[NSString stringWithFormat:@"Payload/%@.app/Assets.car", appName] error:&error];
+    NSString *outPath = [NSString stringWithFormat:@"./%@_ORIGINAL_ASSETS", appName];
+    if (![assetsData writeToFile:outPath options:NSDataWritingAtomic error:&error]) {
+        NSLog(@"Failed to write kernelcache to %s! %s\n", outPath.UTF8String, error.localizedDescription.UTF8String);
+        return false;
+    }
+    return true;
+}
