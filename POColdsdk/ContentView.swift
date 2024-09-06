3cf9d9cf17f6af3e5857064961a1b92e7aa554fa
haxi0 <fabiomailboxgood@mail.ru>
2024-09-06 14:54:13 +0300

    trying to do an automated reddit patcher

--- POColdsdk/ContentView.swift
+++ POColdsdk/ContentView.swift
@@ -17,6 +17,26 @@ struct ContentView: View {
     let restore = Restore.shared
     var body: some View {
         VStack {
+            Button("Automated Reddit Patch") {
+                print("[*] Start")
+                
+                if !restore.getApps().isEmpty {
+                    print("[*] Downloading .car")
+                    
+                    // TODO: Unhardcode :troll:
+                    if grabAssetsCar("https://iosapps.itunes.apple.com/itunes-assets/Purple211/v4/64/21/81/64218131-c1b9-1997-db1d-77c6b1137ffb/extDirgkfjetjcgxeyfbkm.lc.32375868113064696.D6OUVIFQ2FQCA.signed.dpkg.ipa?accessKey=1725657396_7823640340818317029_qnA4o3IYxtaWP6UhOExbFy92AgVGw64gXs9c2fEPljbfkicYPjMLvsJ8t9pUy5jEGu2Mm%2FJMXJCEJKk5XI4Kx9VeL0nhwojUHuYS9aewiqNUmgbQzAnqbiwcoHYwmuI78xC6VsO3wIBmPCR4c3WO66urukVuqYU6mTn0IVcjlSl8u2hoMeeyK16yCs4CeSAl%2F4g%2Bs%2BW3jwCIR6OFzXoJFUG5%2FDP7TW4c%2F732HebNNcgHxWP819CaNXLfjzh7BRI7", "RedditApp") {
+                        print("[*] Replacing with icons with selected png")
+                        do {
+                            try themer.replaceIcons(icon: selectedPng!, car: Bundle.main.resourceURL!.appendingPathComponent("assetbackups/RedditApp_ORIGINAL_ASSETS.car"))
+                        } catch {
+                            print("[!] Failed to patch assets")
+                        }
+                    }
+                } else {
+                    print("[!] getApps is empty")
+                }
+            }
+            .bold()
             Button("Select .car & .png") {
                 isFileImporterPresented = true
             }
